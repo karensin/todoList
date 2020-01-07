6e736fd557a78e01f5bcdf7ef59a5ebdccfdd6db
Karen k  sin <karenksin@MacBook-Pro.attlocal.net>
2020-01-06 18:17:33 -0800

    help

---
+++ src/components /Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+ 
+
+
+class Main extends Component {
+ render() {
+        return (
+          <div> hi </div>
+        );
+    }
+}
+
+
+export default Main;
