ebc635e4e51354de38d1d3fa8ad8de0596080419
Karen k  sin <karenksin@MacBook-Pro.local>
2020-01-17 18:45:37 -0800

    Updates

---
+++ precache-manifest.ffcc6cffbcc6c5a355b43c22ab648927.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b436a848678712ed294330281eae2d4",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "c39f49a01d82068d4163",
+    "url": "/todoList/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "03e797f76267b5a93143",
+    "url": "/todoList/static/css/main.e9810518.chunk.css"
+  },
+  {
+    "revision": "c39f49a01d82068d4163",
+    "url": "/todoList/static/js/2.54c940c1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/todoList/static/js/2.54c940c1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03e797f76267b5a93143",
+    "url": "/todoList/static/js/main.d28a70c0.chunk.js"
+  },
+  {
+    "revision": "8d2486fea3fe6e126662",
+    "url": "/todoList/static/js/runtime-main.c33653bf.js"
+  }
+]);
