fa3388185fa500f9454019dbcdb34e08e9452ce5
Karen k  sin <karenksin@MacBook-Pro.local>
2020-01-17 18:56:04 -0800

    router

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Route, Link } from 'react-router-dom';
-import  Main from './components/Main.js';
+import Main from './components/Main.js';
 
 
 
-ReactDOM.render(  
-    <BrowserRouter>
- 
-        <main>
-     
-        <Route exact path="/" component={Main} />
- 
-         </main>
- 
-    </BrowserRouter>
+ReactDOM.render(
+  <BrowserRouter basename="/todoList">
+
+    <main>
+
+      <Route exact path="/" component={Main} />
+
+    </main>
+
+  </BrowserRouter>
   , document.getElementById('root'))
- 
